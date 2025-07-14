37625eaa463bc018c9530a3ebafcbf53478f9dbd
iYigitOnay <mavi221907@gmail.com>
2025-07-14 12:06:25 +0300

    ÖD
    DY

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var http = require("http");
+var fs = require("fs");
 
 var server = http.createServer((request, response) => {
   if (request.url === "/") {
+    fs.readFile("index.html", (err, data) =>{}
     response.writeHead(200, { "Content-Type": "text/html" });
     response.write();
     response.end();
   } else if (request.url === "/blog") {
     response.writeHead(200, { "content-type": "text/html" });
-
+    response.write();
     response.end();
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
-    response.write("<h1>404 Not Found</h1>");
+    response.write();
     response.end();
   }
 });
