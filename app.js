38fe5c3e0f78d25ab910e786cc10948132525110
iYigitOnay <mavi221907@gmail.com>
2025-07-14 13:01:33 +0300

    ÖD
    DY

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ var fs = require("fs");
 
 var server = http.createServer((request, response) => {
   if (request.url === "/") {
-    fs.readFile("index.html", (err, data) =>{}
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write();
-    response.end();
+    fs.readFile("index.html", (err, html) => {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write();
+      response.end();
+    });
   } else if (request.url === "/blog") {
-    response.writeHead(200, { "content-type": "text/html" });
-    response.write();
-    response.end();
+    fs.readFile("blog.html", (error, html) => {
+      response.writeHead(200, { "content-type": "text/html" });
+      response.write();
+      response.end();
+    });
   } else {
-    response.writeHead(404, { "Content-Type": "text/html" });
-    response.write();
-    response.end();
+    fs.readFile("404.html", (error, html) => {
+      response.writeHead(404, { "Content-Type": "text/html" });
+      response.write();
+      response.end();
+    });
   }
+});
+server.listen(3000, () => {
+  console.log("Sunucu 3000 portunda çalışıyor...");
 });
